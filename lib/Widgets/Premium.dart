d8d029b0e716e07b5b8a1567d802c5d8a8b3a18b
Shreyas20004 <126570047+Shreyas20004@users.noreply.github.com>
2024-03-04 18:06:49 +0530

    New feature development

--- lib/Widgets/Premium.dart
+++ lib/Widgets/Premium.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tms/Widgets/constatant.dart';
 
 class GoPremium extends StatefulWidget {
   const GoPremium({super.key});
@@ -10,6 +11,25 @@ class GoPremium extends StatefulWidget {
 class _GoPremiumState extends State<GoPremium> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+      var he = MediaQuery.of(context).size.height;
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.all(he * 0.004) ,
+          decoration: BoxDecoration(
+            color: kwhite,
+            borderRadius: BorderRadius.circular(he *0.002),
+            gradient: const LinearGradient(
+            colors: [
+            Color (0xffFDC838),
+            Color (0xffF37335),
+            ],
+            begin:Alignment.topLeft,
+            end: Alignment.bottomRight,
+            )
+          ),
+        ),
+      ],
+    );
   }
 }
