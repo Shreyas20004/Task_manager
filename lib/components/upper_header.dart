0c70df716d748168808524990b9ff759173468e8
Shreyas20004 <126570047+Shreyas20004@users.noreply.github.com>
2024-03-05 21:24:04 +0530

    Error fix

--- lib/components/upper_header.dart
+++ lib/components/upper_header.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tms/components/CostumText.dart';
 
-Widget upperHeader(String text,BuildContext context, bool isIcon, required Widget page()){
+Widget upperHeader(String text,BuildContext context, bool isIcon, required Widget page){
   var he = MediaQuery.of(context).size.height;
-  return Padding(padding: EdgeInsets.only(top:he *0.03),);
+  return Padding(padding: EdgeInsets.only(top:he *0.03),;
   child: Row(
     children:[
       GestureDetector(
         onTap: (){
-          Navigator.push(context as BuildContext,MaterialPageRoute builer:(context) =>page); 
+          Navigator.push(context as BuildContext,MaterialPageRoute builder:(context) =>page); 
         },
       )
     ]
   )
+  );
 }
