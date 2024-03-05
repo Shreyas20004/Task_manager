64f8a818aeaa7720825f0a59f6d015bebded63d4
John Doe <johndoe@example.com>
2024-03-05 16:46:43 +0530

    updated

--- lib/Widgets/tasks.dart
+++ lib/Widgets/tasks.dart
@@ -1,14 +1,21 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 
 class Tasks extends StatelessWidget{
+  final tasklist = Task.generateTasks();
   const Tasks({super.key});
 
   @override
   Widget build(BuildContext context){
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 15),
-      child: GridView.builder()
+      child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: 10, crossAxisSpacing: 10), itemBuilder:(context, index)=> )
     );
   }
+}
+
+
+Widget buildAddTask(){
+  return DottedBorder(child: Center(child: Icon(Icons.add, size: 30, Colors.grey.shade600,)), borederType: BorderType.RRect, radius: Radius.circular(10), color: Colors.grey.shade300, strokeWidth: 2, dashPattern: [10, 10])
 }
